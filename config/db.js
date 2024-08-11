6565d20376d7517557058119d7ae8a195e7a7286
Parthiban <137680054+Parthiban-Saravanan@users.noreply.github.com>
2024-08-12 04:28:40 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,9 +1,10 @@
+// config/db.js
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    // Using the MONGO_URI from the environment variables
-    await mongoose.connect('mongodb://localhost:27017/password-reset', {
+    // Use the MONGO_URI from the environment variables
+    await mongoose.connect('mongodb+srv://parthibanaakash:5RoHizCmrSiudZsQ@cluster0.ozwfygp.mongodb.net/', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
